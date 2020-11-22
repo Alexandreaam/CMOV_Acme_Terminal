31a9d71441691974d424970b2025ab7ec6779936
Alexandreaam <alex.al.mar@hotmail.com>
2020-11-22 00:00:41 +0000

    First Reader Version

---
+++ app/src/main/java/com/acme_industries/cmov_acme_terminal/CaptureActivity.kt
@@ -0,0 +1,4 @@
+package com.acme_industries.cmov_acme_terminal
+import com.journeyapps.barcodescanner.CaptureActivity
+
+class CaptureActivity : CaptureActivity()
---
+++ app/src/main/java/com/acme_industries/cmov_acme_terminal/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.acme_industries.cmov_acme_terminal
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG: String = "TAG"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.message)
+        button.setOnClickListener {
+            scanQRCode()
+        }
+    }
+
+    private fun scanQRCode(){
+        val integrator = IntentIntegrator(this).apply {
+            captureActivity = CaptureActivity::class.java
+            setOrientationLocked(false)
+            setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+            setPrompt("Scanning Code")
+        }
+        integrator.initiateScan()
+    }
+
+    // Get the results:
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            else Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
