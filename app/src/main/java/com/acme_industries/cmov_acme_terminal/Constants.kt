89e2eaee63408f3b878d304c266ae33becc43be1
Alexandre <alex.al.mar@hotmail.com>
2020-11-22 01:19:49 +0000

    QR codes are now read and errors are handled

---
+++ app/src/main/java/com/acme_industries/cmov_acme_terminal/Constants.kt
@@ -0,0 +1,7 @@
+package com.acme_industries.cmov_acme_terminal
+
+class Constants {
+    companion object {
+        const val serverUrl = "http://10.0.2.2:3000/"
+    }
+}
--- app/src/main/java/com/acme_industries/cmov_acme_terminal/MainActivity.kt
+++ app/src/main/java/com/acme_industries/cmov_acme_terminal/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.google.zxing.integration.android.IntentIntegrator
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,9 +40,42 @@ class MainActivity : AppCompatActivity() {
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         if (result != null) {
             if (result.contents == null) Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
-            else Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+            else parseRequest(result.contents)
         } else {
             super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun parseRequest(message: String) {
+        try {
+            val contents = JSONObject(message)
+            if(contents.has("Products") && contents.has("Vouchers")
+                && contents.has("userid") && contents.has("Total")){
+
+                val url = Constants.serverUrl + "order"
+                val queue = Volley.newRequestQueue(this)
+
+                //TODO (Simplify translation to json array)
+                val orderTest = JSONObject()
+                orderTest.put("Products",contents.get("Products"))
+                orderTest.put("Vouchers",contents.get("Vouchers"))
+                orderTest.put("userid", contents.get("userid"))
+                orderTest.put("Total", contents.get("Total"))
+
+                val jsonObjectRequest = JsonObjectRequest(
+                    Request.Method.POST, url, orderTest ,
+                    { response ->
+                        println("Response is: $response")
+                    },
+                    { error ->
+                        println("That didn't work: ${error.message}")
+                    })
+                queue.add(jsonObjectRequest)
+            } else {
+                Toast.makeText(this, "Invalid QR Code", Toast.LENGTH_LONG).show()
+            }
+        } catch (e: Exception) {
+            Toast.makeText(this, "Invalid QR Code", Toast.LENGTH_LONG).show()
         }
     }
 }
